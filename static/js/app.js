486a78dffdc46e7b7996a8d0c077a8fc4e2fc6f0
EJBaquet <ejbaquet@yahoo.com>
2020-08-31 13:14:32 -0400

    removed select event from html; add new event and metadata filter based on select

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,10 @@
+// Pull in and process samples.json for charts
 d3.json("samples.json").then((data) => {
+  // retrieve datasets
   var metadata = data.metadata;
   var subjectNames = data.names;
+
+  // console.log(metadata);
 
   // add test subjects to select
   var testSubject = d3.select("#selDataset");
@@ -8,4 +12,29 @@ d3.json("samples.json").then((data) => {
     var newSubject = testSubject.append("option");
     newSubject.text(subject);
   });
+
+  // define elements
+  var sampleMetadata = d3.select("#sample-metadata");
+
+  // change event for select
+  testSubject.on("change", updateCharts);
+
+  // event functions
+  function updateCharts() {
+    // Prevent refresh
+    d3.event.preventDefault();
+
+    // get selectedSubject
+    var selectedSubject = testSubject.property("value");
+
+    // filter metadata for panel-body
+    filteredMetadata = metadata.filter(function (meta) {
+      return meta.id == selectedSubject;
+    });
+    var strFiltMetadata = JSON.stringify(filteredMetadata);
+
+    // console.log
+    console.log(`Selected Sample: ${selectedSubject}`);
+    console.log(`Sample Metadata: ${strFiltMetadata}`);
+  }
 });
